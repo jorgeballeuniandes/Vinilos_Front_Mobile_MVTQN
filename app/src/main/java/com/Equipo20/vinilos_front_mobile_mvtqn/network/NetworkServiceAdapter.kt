1ea76e16c14e45809087ca1aaede13e26708edbf
esandher <e.sandovalh@uniandes.edu.co>
2022-11-04 18:08:12 -0500

    Fix: Se cambia mínima versión SDK de 24 a 22 (Android Lollipop 5.1)

--- app/src/main/java/com/Equipo20/vinilos_front_mobile_mvtqn/network/NetworkServiceAdapter.kt
+++ app/src/main/java/com/Equipo20/vinilos_front_mobile_mvtqn/network/NetworkServiceAdapter.kt
@@ -17,7 +17,7 @@ import org.json.JSONObject
 
 class NetworkServiceAdapter constructor(context: Context) {
     companion object{
-        const val BASE_URL= "https://back-vinyls-populated.herokuapp.com/"
+        const val BASE_URL= "https://vinilos-back-end.herokuapp.com/"
         var instance: NetworkServiceAdapter? = null
         fun getInstance(context: Context) =
             instance ?: synchronized(this) {
@@ -93,8 +93,5 @@ class NetworkServiceAdapter constructor(context: Context) {
             }))
 
     }
-
-
-
 
 }
