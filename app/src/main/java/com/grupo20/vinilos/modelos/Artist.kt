132e1149f15923e2a19be04c7c4c57a50cf91fac
esandher <e.sandovalh@uniandes.edu.co>
2022-11-19 13:02:11 -0500

    Feat: Detalle artista

--- app/src/main/java/com/grupo20/vinilos/modelos/Artist.kt
+++ app/src/main/java/com/grupo20/vinilos/modelos/Artist.kt
@@ -2,6 +2,7 @@ package com.grupo20.vinilos.modelos
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.util.Date
 
 
 @Parcelize
@@ -9,5 +10,6 @@ data class Artist(
     val artistId: Int,
     val name:String,
     val image:String,
-    val description:String
+    val description:String,
+    //val birthDate:Date
 ): Parcelable
--- app/src/main/java/com/grupo20/vinilos/ui/artists/detail_artist/ArtistDetailFragment.kt
+++ app/src/main/java/com/grupo20/vinilos/ui/artists/detail_artist/ArtistDetailFragment.kt
@@ -1,21 +1,33 @@
 package com.grupo20.vinilos.ui.artists.detail_artist
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.Image
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.imageview.ShapeableImageView
 import com.grupo20.vinilos.R
+import com.grupo20.vinilos.ui.artists.ArtistFragmentDirections
+import java.util.concurrent.Executors
 
 class ArtistDetailFragment : Fragment() {
 
     private val args: ArtistDetailFragmentArgs by navArgs()
-    val artist = args.artist
+    private val artist by lazy{
+        args.artist
+    }
     companion object {
         fun newInstance() = ArtistDetailFragment()
     }
@@ -37,10 +49,37 @@ class ArtistDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val Nombre: TextView = view.findViewById(R.id.Nombre)
-        val Imagen:ShapeableImageView = view.findViewById(R.id.image_text)
-        Nombre.text = artist.name
-        //Imagen.setBackgroundDrawable  (artist.image)
+        val nombre: TextView = view.findViewById(R.id.nombre)
+        val descripcion: TextView = view.findViewById(R.id.descripcion)
+        val birth: TextView = view.findViewById(R.id.birth)
+        val imagen: ImageView = view.findViewById(R.id.foto)
+        val back: Button = view.findViewById(R.id.back)
+        var navigator: NavController = findNavController()
+
+        nombre.text = artist.name
+        descripcion.text = artist.description
+
+        back.setOnClickListener {
+            val action = ArtistDetailFragmentDirections.actionArtistDetailFragmentToNavigationArtists()
+            navigator?.navigate(action)
+        }
+
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+        var image: Bitmap? = null
+
+        executor.execute {
+            try {
+                val `in` = java.net.URL(artist.image).openStream()
+                image = BitmapFactory.decodeStream(`in`)
+                handler.post {
+                    imagen.setImageBitmap(image)
+                }
+            }
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
 
