782c1b1d6aeb085618490688894898c3346dc3a1
Afiffe Retamozo <afiffe.retamozo.a@gmail.com>
2022-11-20 13:32:25 -0500

    Adding the array adapters

--- app/src/main/java/com/grupo20/vinilos/ui/albums/create_album/CreateAlbumFragment.kt
+++ app/src/main/java/com/grupo20/vinilos/ui/albums/create_album/CreateAlbumFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import com.grupo20.vinilos.R
 
 class CreateAlbumFragment : Fragment() {
@@ -23,7 +24,17 @@ class CreateAlbumFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_create_album, container, false)
+        val createview = inflater.inflate(R.layout.fragment_create_album, container, false)
+        val generosArray = resources.getStringArray(R.array.list_generos)
+        val autoCompleteGeneros = createview.findViewById<AutoCompleteTextView>(R.id.autoComplete_generos)
+        val adapter = ArrayAdapter(requireContext(),android.R.layout.simple_expandable_list_item_1, generosArray)
+        autoCompleteGeneros?.setAdapter(adapter)
+
+        val recordsArray = resources.getStringArray(R.array.list_records)
+        val autoCompleteRecords = createview.findViewById<AutoCompleteTextView>(R.id.autoComplete_records)
+        val recordAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_expandable_list_item_1, recordsArray)
+        autoCompleteRecords?.setAdapter(recordAdapter)
+        return createview
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
